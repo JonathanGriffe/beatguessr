66142a0e0f27ad7b3b744f13cc7bb99b942d5e5b
Jonathan Griffe <jonathan.griffe@gmail.com>
2025-11-14 13:55:33 +0100

    feat: add user imported playlists

--- backend/quiz/services/playlist.py
+++ backend/quiz/services/playlist.py
@@ -1,24 +1,30 @@
 import json
+import logging
 
 from accounts.services.auth import get
 from core.settings import BASE_DIR
+from quiz.constants import PLAYLIST_SIZE_LIMIT
 from quiz.models.playlist import Playlist
 from quiz.models.song import Song
 
+logger = logging.getLogger(__name__)
 
-def get_items(playlist_id):
+
+def get_items(playlist_id, user=None):
     item_fields = "next,items(track(name,id,popularity,artists(name),album(images(url))))"
     fields = f"name, images(url),tracks({item_fields})"
-    data = get(f"https://api.spotify.com/v1/playlists/{playlist_id}?market=FR&fields={fields}").json()
+    data = get(f"https://api.spotify.com/v1/playlists/{playlist_id}?market=FR&fields={fields}&limit=100", user).json()
     tracks = [item["track"] for item in data["tracks"]["items"]]
     image_link = data["images"][0]["url"]
     name = data["name"]
 
     next_url = data["tracks"]["next"]
+    i = 0
     if next_url:
         next_url = f"{next_url.split('fields=')[0]}fields={item_fields}"
-        while next_url:
-            response = get(next_url)
+        while next_url and i < PLAYLIST_SIZE_LIMIT:
+            i += 1
+            response = get(next_url, user)
             data = response.json()
             for track in data["items"]:
                 tracks.append(track["track"])
@@ -27,9 +33,9 @@ def get_items(playlist_id):
     return name, image_link, tracks
 
 
-def import_playlist(playlist_id, category):
+def import_playlist(playlist_id, category, user=None):
     song_ids = []
-    name, image_link, tracks = get_items(playlist_id)
+    name, image_link, tracks = get_items(playlist_id, user)
     for track in tracks:
         song, created = Song.objects.get_or_create(
             spotify_id=track["id"],
@@ -43,8 +49,21 @@ def import_playlist(playlist_id, category):
         song_ids.append(song.id)
 
     playlist = Playlist.objects.create(spotify_id=playlist_id, title=name, category=category, image_link=image_link)
+    if user:
+        playlist.users.add(user)
     playlist.songs.set(song_ids)
 
+    return playlist
+
+
+def import_playlist_for_user(playlist_id, user):
+    if Playlist.objects.filter(spotify_id=playlist_id).exists():
+        playlist = Playlist.objects.get(spotify_id=playlist_id)
+        playlist.users.add(user)
+        logger.info("Added user to playlist", extra={"playlist_id": playlist.id, "user_id": user.id})
+    else:
+        playlist = import_playlist(playlist_id, "My Playlists", user)
+        logger.info("Imported playlist for user", extra={"playlist_id": playlist.id, "user_id": user.id})
     return playlist
 
 
