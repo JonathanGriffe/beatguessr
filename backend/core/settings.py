b80c909c8f0886b2d013d86f9d54b145dc6ea8ea
Jonathan Griffe <jonathan.griffe@gmail.com>
2025-11-21 18:08:56 +0100

    test: fix tests

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -71,7 +71,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("redis", 6379)],
+            "hosts": [(os.environ.get("REDIS_HOST", "redis"), 6379)],
         },
     },
 }
@@ -112,7 +112,7 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://redis:6379/1",
+        "LOCATION": f"redis://{os.environ.get('REDIS_HOST', 'redis')}:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
--- backend/quiz/tests/views/test_playlist.py
+++ backend/quiz/tests/views/test_playlist.py
@@ -11,13 +11,13 @@ def test_playlist_view():
     playlist3 = Playlist.objects.create(title="test3", category="other", image_link="", spotify_id="3")
 
     client = APIClient()
-    client.force_authenticate(user=CustomUser(username="test", password="test"))
+    client.force_authenticate(user=CustomUser.objects.create_user(username="test", password="test"))
     response = client.get("/quiz/playlists/")
     assert response.status_code == 200
     assert response.json() == {
         "test": [
-            {"id": playlist1.id, "title": "test", "image_link": ""},
-            {"id": playlist2.id, "title": "test2", "image_link": ""},
+            {"id": playlist1.spotify_id, "title": "test", "image_link": ""},
+            {"id": playlist2.spotify_id, "title": "test2", "image_link": ""},
         ],
-        "other": [{"id": playlist3.id, "title": "test3", "image_link": ""}],
+        "other": [{"id": playlist3.spotify_id, "title": "test3", "image_link": ""}],
     }
