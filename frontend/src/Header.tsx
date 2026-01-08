ae976676a916d636acf2de0964a722f27ac53506
Jonathan Griffe <jonathan.griffe@gmail.com>
2026-01-08 16:10:14 +0100

    feat: change UI theme

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -2,10 +2,10 @@ import { Music } from 'lucide-react';
 import { Link } from 'react-router';
 
 function Header() {
-    return <header className="flex flex-row justify-left items-center bg-cred text-darkblue font-bold text-2xl">
+    return <header className="flex flex-row justify-left items-center bg-beige text-darkblue font-bold text-2xl border-b-2 border-lighterblue pl-6">
         <Link to='/' className="flex flex-row p-1 items-center">
-            <Music size="40"/>
-            <h1>BeatGuessr</h1>
+            <Music size="40" />
+            <h1>Beat<span className="text-greenblue">G</span>uessr</h1>
         </Link>
     </header>
 }
--- frontend/src/PlaylistCard.tsx
+++ frontend/src/PlaylistCard.tsx
@@ -1,21 +1,19 @@
 import { Link } from "react-router"
-import { Button } from "./components/ui/button"
 
 export interface Playlist {
-    id: number,
-    title: string,
-    image_link: string
+  id: number,
+  title: string,
+  image_link: string
 }
 
-function PlaylistCard (props: {playlist: Playlist}) {
-    return (
-        <Link to={`/quiz/?playlist_id=${props.playlist.id}`}>
-          <Button className="rounded-xl border-corange border-2 bg-beige hover:bg-darkblue hover:text-beige w-60 h-60 flex flex-col text-corange">
-            <img src={props.playlist.image_link} className="flex-1 min-w-full "/>
-            <span>{props.playlist.title}</span>
-          </Button>
-        </Link>
-    )
+function PlaylistCard(props: { playlist: Playlist }) {
+  return (
+    <Link to={`/quiz/?playlist_id=${props.playlist.id}`}>
+      <div style={{ backgroundImage: `url(${props.playlist.image_link})` }} className="bg-cover rounded-xl border-lighterblue border-6 hover:border-corange hover:text-beige w-60 h-60 flex justify-center items-center text-corange">
+        <span className="p-1 bg-white/50 backdrop-blur-md rounded-lg text-2xl font-bold text-lighterblue">{props.playlist.title}</span>
+      </div>
+    </Link>
+  )
 }
 
 
