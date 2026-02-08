5c3ad3648bedd87fe2e3bc990fff2a3f5be6c77e
Jonathan Griffe <jonathan.griffe@gmail.com>
2026-02-08 14:50:21 +0100

    feat: migrate away from spotify authentication

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,10 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 
 class CustomUser(AbstractUser):
-    spotify_id = models.CharField(max_length=64, unique=True)
-    name = models.CharField(max_length=64)
-    access_token = models.CharField(max_length=1024, blank=True, null=True)
-    refresh_token = models.CharField(max_length=1024, blank=True, null=True)
-    token_expires = models.DateTimeField(blank=True, null=True)
+    pass
